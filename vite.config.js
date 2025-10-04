a3243b8e0155f1c9307fda171d4c42e466043147
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2025-10-04 16:20:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // CHANGE THIS ⬇️ to your GitHub repo name
-const repoName = 'portfolio-site'  // <-- example, change if yours is different
+  // <-- example, change if yours is different
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,   // 👈 this fixes GitHub Pages blank page
+  base: '/portfolio-site/',   // 👈 this fixes GitHub Pages blank page
 
   server: {
     host: '0.0.0.0',
